5f31fad0557891f0bce92ada82ea68a74f801610
christophe.wouters.gspace@gmail.com <christophe.wouters.gspace@gmail.com@7dd9900a-a8df-11de-a118-c7387b1e4f66>
2012-05-09 09:04:29 +0000

    Full FPS

--- trunk/DawnClient/AvatarCommand.cs
+++ trunk/DawnClient/AvatarCommand.cs
@@ -7,16 +7,17 @@ namespace DawnClient
 {
     public enum AvatarCommand
     {
-        TurnRight,
-        TurnLeft,
-        TurnRightSlow,
-        TurnLeftSlow,
-        RunForward,
-        WalkForward,
-        WalkBackward,
-        StrafeRight,
-        StrafeLeft,
-        Fire,
-        FireRocket
+        Unknown = 0,
+        TurnRight = 1,
+        TurnLeft = 2,
+        TurnRightSlow = 3,
+        TurnLeftSlow = 4,
+        RunForward = 5,
+        WalkForward = 6,
+        WalkBackward = 7,
+        StrafeRight = 8,
+        StrafeLeft = 9,
+        Fire = 10,
+        FireRocket = 11
     }
 }
