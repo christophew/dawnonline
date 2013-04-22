e1f1b6a76a7e9265145b1f836a85798c644687a5
christophe.wouters.gspace <christophe.wouters.gspace@7dd9900a-a8df-11de-a118-c7387b1e4f66>
2013-04-22 20:31:32 +0000

    Unity: camera movement & debug info on creature position

---
+++ DawnUnity/Assets/Scripts/CreatureScript.cs
@@ -0,0 +1,49 @@
+using System;
+using DawnClient;
+using UnityEngine;
+using System.Collections;
+
+public class CreatureScript : MonoBehaviour
+{
+    public DawnClientEntity Entity; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+        if (Entity == null)
+            return;
+
+        // Update position & angle
+        //{
+        //    transform.position = new Vector3(Entity.PlaceX, 0, Entity.PlaceY);
+
+        //    // Why do we need a negative angle?
+        //    transform.eulerAngles = new Vector3(0, (float) RadianToDegree(-Entity.Angle), 0);
+        //}
+    }
+
+    void OnGUI()
+    {
+        if (Entity == null)
+            return;
+
+        var screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        var labelRect = new Rect(screenPos.x, Screen.height - screenPos.y, Screen.width, Screen.height);
+
+        //var label = string.Format("({0}, {1})", (int)transform.position.x, (int)transform.position.z);
+        var label = Entity.DamagePercent + "/" + Entity.ResourcePercent;
+
+        GUI.Label(labelRect, label);
+    }
+
+    private static double RadianToDegree(double angle)
+    {
+        return angle * (180.0 / Math.PI);
+    }
+}
