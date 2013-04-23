f385af0d630c34b8f4d6f81149aa7c5f7b35c350
christophe.wouters.gspace <christophe.wouters.gspace@7dd9900a-a8df-11de-a118-c7387b1e4f66>
2013-04-23 15:41:13 +0000

    Some fun with unity: shadows, torpedo's, explosions, ...

---
+++ trunk/DawnUnity/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour
+{
+
+    public float TimeToLive = 1;
+
+    private float _startTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Time.time > _startTime + TimeToLive)
+	    {
+	        Destroy(gameObject);
+	    }
+	}
+}
---
+++ trunk/DawnUnity/Assets/Scripts/SetColor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class SetColor : MonoBehaviour
+{
+    public Color Color;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (renderer != null)
+        {
+            renderer.material.color = Color;
+        }
+
+	}
+}
