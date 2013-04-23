dcfa332a35fe978847b2ca5c209cf68a98e1c7a8
christophe.wouters.gspace <christophe.wouters.gspace@7dd9900a-a8df-11de-a118-c7387b1e4f66>
2013-04-23 11:15:27 +0000

    Unity port + lerp + opts

---
+++ DawnUnity/Assets/Scripts/EntityScript.cs
@@ -0,0 +1,80 @@
+using System;
+using DawnClient;
+using UnityEngine;
+using System.Collections;
+
+public class EntityScript : MonoBehaviour {
+
+    public DawnClientEntity Entity;
+
+    private LerpInfo _positionLerp;
+    private LerpInfo _angleLerp;
+
+	// Use this for initialization
+	void Start ()
+	{
+        _positionLerp = new LerpInfo(transform.position, 5);
+        _angleLerp = new LerpInfo(transform.eulerAngles, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Invert z-axis = convert left-handed to right-handed orientation
+        //transform.position = new Vector3(Entity.PlaceX, 0, -Entity.PlaceY);
+        //transform.eulerAngles = new Vector3(0, (float)RadianToDegree(Entity.Angle), 0);
+
+        var newPosition = new Vector3(Entity.PlaceX, 0, -Entity.PlaceY);
+        var newAngle = new Vector3(0, (float)RadianToDegree(Entity.Angle), 0);
+
+        transform.position = _positionLerp.UpdateLerp(newPosition);
+        transform.eulerAngles = _angleLerp.UpdateLerp(newAngle);
+    }
+
+    private static double RadianToDegree(double angle)
+    {
+        return angle * (180.0 / Math.PI);
+    }
+}
+
+class LerpInfo
+{
+    private Vector3 _endMarker;
+    private Vector3 _current;
+
+    private Vector3 _step;
+    private int _stepCounter;
+
+    private int _nrOfSteps = 1;
+
+    public LerpInfo(Vector3 start, int nrOfStep)
+    {
+        _current = start;
+        _nrOfSteps = nrOfStep;
+
+        SetNewTarget(start);
+    }
+
+    private void SetNewTarget(Vector3 newTarget)
+    {
+        if (newTarget == _endMarker)
+            return;
+
+        _endMarker = newTarget;
+
+        _step = (_endMarker - _current) / _nrOfSteps;
+        _stepCounter = 0;
+    }
+
+    public Vector3 UpdateLerp(Vector3 newTarget)
+    {
+        SetNewTarget(newTarget);
+
+        if (_stepCounter >= _nrOfSteps)
+            return _current;
+
+        _current += _step;
+        _stepCounter++;
+
+        return _current;
+    }
+}
