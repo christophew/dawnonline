d9af525aa95290d0a28b061374d567391cb226a3
christophe.wouters.gspace@gmail.com <christophe.wouters.gspace@gmail.com@7dd9900a-a8df-11de-a118-c7387b1e4f66>
2014-11-18 12:32:02 +0000

    sigmoid experiments

--- trunk/AgentMatrix/Factories/NeuralConfiguration.cs
+++ trunk/AgentMatrix/Factories/NeuralConfiguration.cs
@@ -9,10 +9,12 @@ namespace DawnOnline.AgentMatrix.Factories
     public static class NeuralConfiguration
     {
         internal static Node.NodeTypeEnum NodeType = Node.NodeTypeEnum.Linear;
+        internal static double SigmoidSlope = 1.0;
+        internal static double SigmoidScale = 25.0;
 
         public static void UseSigmoidNodes()
         {
-            NodeType = Node.NodeTypeEnum.Signoid;
+            NodeType = Node.NodeTypeEnum.Sigmoid;
         }
     }
 }
